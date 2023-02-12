1bcbe407ad93a65640d8d1a46f3bfe4cb44f8ce9
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-02-11 20:58:36 -0500

    Add files via upload
    includes reference to valuechange module and basic terminal prompts. still needs way to choose file thru terminal

---
+++ main.py
@@ -0,0 +1,80 @@
+import numpy as np
+from PIL import Image
+import matplotlib as plt
+from valuechange import cb_compatible
+
+Deuteranomaly0_5 = (0.547494, 0.607765, -0.155259, 0, 0.181692, 0.781742, 0.036566, 0, -0.010410, 0.027275, 0.983136, 0)
+Protanomaly_05 = (0.458064, 0.679578, -0.137642, 0, 0.092785, 0.846313, 0.060902, 0, -0.007494, -0.016807, 1.024301, 0)
+Tritanomaly0_5 = (1.017277, 0.027029, -0.044306, 0, -0.006113, 0.958479, 0.047634, 0, 0.006379, 0.248708, 0.744913, 0)
+
+
+def color_to_cb(cb_type, im):
+    # converts normal image to specified colorblind spectrum
+    out = im.convert("RGB", cb_type, None, 0,256)
+    out.show()
+
+im = (Image.open('brain_chico_crop.jpeg'))
+im.show()
+
+# Convert PNG to JPEG
+
+program_run = True
+    
+while program_run == True: 
+    print("Choose which type of color-blindness to simulate: \n")
+    print("Protanomaly:        p")
+    print("Deuteranomaly:      d")
+    print("Tritanomaly:        t")
+    print("Exit:               e")
+
+    try:
+        user_input = str(input())  
+    except TypeError:
+        print("Not a valid input, try again!")
+    
+    if user_input == "p":
+        cb_type = Protanomaly_05
+        color_to_cb(cb_type, im)
+        
+    if user_input == "d":
+        cb_type = Deuteranomaly0_5
+        color_to_cb(cb_type, im)
+        
+    if user_input == "t":
+        cb_type = Tritanomaly0_5
+        color_to_cb(cb_type, im)
+        
+    if user_input == "e":
+        print("Goodbye!")
+        break
+    
+    try:
+        convert_input = str(input("Make CB friendly? y/n"))
+        if convert_input == "y":
+            mod_im = cb_compatible(im)
+            mod_im.show()
+            try: 
+                preview_input = str(input("CB Preview of modified image? y/n"))
+                if preview_input == "y":
+                    color_to_cb(cb_type, mod_im)
+                if preview_input == "n":
+                    continue
+            except TypeError:
+                print("Not a valid input!")
+        if convert_input == "n":
+            continue
+        else: 
+            print("Not a valid input, try again!")
+    except TypeError:
+        print("Not a valid input, try again!")
+        
+        
+    try:
+        again_input = str(input("Again? y/n"))
+        if again_input == "y":
+            program_run = True
+        if again_input == "n":
+            print("Goodbye!")
+            program_run = False
+    except TypeError:
+        print("Not a valid input, try again!")
