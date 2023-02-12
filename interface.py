9aca9f34f90c1a32eb667724a9f803aa1382542e
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-02-12 08:21:00 -0500

    Add files via upload
    Generates colorblind simulation of hue-shifted image

--- interface.py
+++ interface.py
@@ -1,22 +1,70 @@
 import tkinter as tk
+import numpy as np
 from tkinter import filedialog
 from PIL import Image, ImageTk
+from valuechange import cb_compatible
+import os
 
-
-def convert(filename):
+def color_to_cb(c_type,filename):
     a = 1
+    Neutral = (1, 0,0 , 0, 0, 1, 0, 0, 0, 0, 1, 0)
     Deuteranomaly0_5 = (0.547494, 0.607765, -0.155259, 0, 0.181692, 0.781742, 0.036566, 0, -0.010410, 0.027275, 0.983136, 0)
-    Deuteranomaly0 = (1, 0,0 , 0, 0, 1, 0, 0, 0, 0, 1, 0)
     Deuteranomaly1 = (.367322, .860646, -.227968 , 0, .280085, .672501, .047413, 0, -.01182, .04294, .968881, 0)
-    im = (Image.open('pencils.jpg'))
-    out = im.convert("RGB", Deuteranomaly0, None, 0,256)
-    out = out.save(str(a) + '.jpg')
-    out = im.convert("RGB", Deuteranomaly0_5, None, 0,256)
-    a += 1
-    out = out.save(str(a) + '.jpg')
-    out = im.convert("RGB", Deuteranomaly1, None, 0,256)
-    a += 1
-    out = out.save(str(a) + '.jpg')
+    Protanomaly0_5 = (0.458064, 0.679578, -0.137642, 0, 0.092785, 0.846313, 0.060902, 0, -0.007494, -0.016807, 1.024301, 0)
+    Protanomaly1 = (.152286, 1.052583, -.204868 , 0, .114503, .786281, .099216, 0, -.003882, -.048116, 1.051998, 0)
+    Tritanomaly0_5 = (1.017277, 0.027029, -0.044306, 0, -0.006113, 0.958479, 0.047634, 0, 0.006379, 0.248708, 0.744913, 0)
+    Tritanomaly1 = (1.255528, -0.076749, -0.178779, 0, -0.078411, 0.930809, 0.147602, 0, 0.004733, 0.691367, 0.303900, 0)
+    im = (Image.open(filename))
+    if(c_type == "Deuteranomaly"):
+        out = im.convert("RGB", Neutral, None, 0,256)
+        out = out.save(str(a) + '.jpg')
+        out = im.convert("RGB", Deuteranomaly0_5, None, 0,256)
+        a += 1
+        out = out.save(str(a) + '.jpg')
+        out = im.convert("RGB", Deuteranomaly1, None, 0,256)
+        a += 1
+        out = out.save(str(a) + '.jpg')
+    if(c_type == "Protanomaly"):
+        out = im.convert("RGB", Neutral, None, 0,256)
+        out = out.save(str(a) + '.jpg')
+        out = im.convert("RGB", Protanomaly0_5, None, 0,256)
+        a += 1
+        out = out.save(str(a) + '.jpg')
+        out = im.convert("RGB", Protanomaly1, None, 0,256)
+        a += 1
+        out = out.save(str(a) + '.jpg')
+    if(c_type == "Tritanomaly"):
+        out = im.convert("RGB", Neutral, None, 0,256)
+        out = out.save(str(a) + '.jpg')
+        out = im.convert("RGB", Tritanomaly0_5, None, 0,256)
+        a += 1
+        out = out.save(str(a) + '.jpg')
+        out = im.convert("RGB", Tritanomaly1, None, 0,256)
+        a += 1
+        out = out.save(str(a) + '.jpg')
+def finalConversion(imgaging, c_type):
+    Deuteranomaly0_5 = (0.547494, 0.607765, -0.155259, 0, 0.181692, 0.781742, 0.036566, 0, -0.010410, 0.027275, 0.983136, 0)
+    Protanomaly0_5 = (0.458064, 0.679578, -0.137642, 0, 0.092785, 0.846313, 0.060902, 0, -0.007494, -0.016807, 1.024301, 0)
+    Tritanomaly0_5 = (1.017277, 0.027029, -0.044306, 0, -0.006113, 0.958479, 0.047634, 0, 0.006379, 0.248708, 0.744913, 0)
+    
+    if c_type == "Deuteranomaly": 
+        out = imgaging.convert("RGB", Deuteranomaly0_5, None, 0,256)
+        out = out.save('inclusive.jpg')
+    
+    if c_type == "Protanomaly": 
+        out = imgaging.convert("RGB", Protanomaly0_5, None, 0,256)
+        out = out.save('inclusive.jpg')
+
+    if c_type == "Tritanomaly": 
+        out = imgaging.convert("RGB", Tritanomaly0_5, None, 0,256)
+        out = out.save('inclusive.jpg')
+    os.remove("1.jpg")
+    os.remove("2.jpg")
+    os.remove("3.jpg")
+    window.destroy()
+    
+    
+
 
 def browseFiles():
     
@@ -26,10 +74,18 @@ def browseFiles():
       
     # Change label contents
     # label_file_explorer.configure(text="File Opened: "+filename)
-    open(filename)
+    buttonDeut = tk.Button(window, text="Deuteranomaly",command=lambda: open("Deuteranomaly", filename))
+    
+    buttonProt = tk.Button(window, text="Protanomaly",command=lambda: open("Protanomaly",filename))
+   
+    buttonTrit = tk.Button(window, text="Tritanomaly",command=lambda: open("Tritanomaly",filename))
+    buttonDeut.grid(row=1,column = 0)
+    buttonProt.grid(row=2,column = 0)
+    buttonTrit.grid(row=3,column = 0)
+    
 
 
-def open(filename):
+def open(c_type,filename):
     global my_label
     global img
     top = tk.Toplevel()
@@ -39,7 +95,7 @@ def open(filename):
     img = ImageTk.PhotoImage(Image.open(filename))
     my_label = tk.Label(top, image=img)
     my_label.grid(row=0,column =0, columnspan=3)
-    convert(filename)
+    color_to_cb(c_type,filename)
     img1 = ImageTk.PhotoImage(Image.open('1.jpg'))
     img2 = ImageTk.PhotoImage(Image.open('2.jpg'))
     img3 = ImageTk.PhotoImage(Image.open('3.jpg'))
@@ -56,7 +112,7 @@ def open(filename):
         my_label = tk.Label(image=image_list[num-1])
         button_forward = tk.Button(top, text=">>", command = lambda: forward(num+1))
         button_back = tk.Button(top, text="<<", command=lambda: back(num - 1))
-        heading = tk.Label(top, text= "Deutoronamaly: " + str((num - 1) * 0.5))
+        heading = tk.Label(top, text= c_type + str((num - 1) * 0.5))
         heading.grid(row = 0, column =0)
         if num == 3: 
             button_forward = tk.Button(top, text=">>", state="disabled")
@@ -73,7 +129,7 @@ def open(filename):
         my_label = tk.Label(image=image_list[num-1])
         button_forward = tk.Button(top, text=">>", command = lambda: forward(num+1))
         button_back = tk.Button(top, text="<<", command=lambda: back(num - 1))
-        heading = tk.Label(top, text= "Deutoronamaly: " + str((num - 1) * 0.5))
+        heading = tk.Label(top, text= c_type + str((num - 1) * 0.5))
         heading.grid(row = 0, column =0)
         if num == 1: 
             button_back = tk.Button(top, text="<<", state="disabled")
@@ -81,12 +137,34 @@ def open(filename):
         my_label.grid(row=0,column =0, columnspan=3)
         button_back.grid(row = 1, column = 0)
         button_forward.grid(row = 1, column = 2)
+    def call_cb_compatible(color):
+        global im
+        global show
+        global converted_img
+        im = Image.open(filename)
+        show = cb_compatible(im, color)
+        show.save("color_adjusted.jpg")
+        converted_img = ImageTk.PhotoImage(show)
+        top2 = tk.Toplevel()
+        top2.title('Converted:' + color)
+        this_label = tk.Label(top2, image=converted_img)
+        this_label.grid()
+        button_save = tk.Button(top2, text="inclusive modification", command= lambda: finalConversion(show, c_type))
+        button_nope = tk.Button(top2, text="no", command = lambda : top2.destroy())
+        button_save.grid()
+        button_nope.grid()
 
     button_back = tk.Button(top, text = "<<", command = back, state = "disabled")
     button_out = tk.Button(top, text = "exit", command = top.quit)
+    button_convert_red = tk.Button(top, text="Hue Shift Red", command=lambda:call_cb_compatible("r"))
+    button_convert_blue = tk.Button(top, text="Hue Shift Blue", command=lambda:call_cb_compatible("b"))
+    button_convert_green = tk.Button(top, text="Hue Shift Green", command=lambda:call_cb_compatible("g"))
     button_forward = tk.Button(top, text = ">>", command = lambda: forward(2))
 
     button_back.grid(row = 1, column = 0)
+    button_convert_red.grid(row = 2, column =1)
+    button_convert_blue.grid(row = 2, column =2)
+    button_convert_green.grid(row = 2, column =3)
     button_out.grid(row = 1, column = 1)
     button_forward.grid(row = 1, column = 2)
 
