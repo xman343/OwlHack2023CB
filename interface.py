384d541fe125f7fd7d104d68d5b4bb7ee0cd7324
Xander Leatherwood <44583237+xman343@users.noreply.github.com>
2023-02-12 09:55:22 -0500

    Add files via upload
    finishing touches

--- interface.py
+++ interface.py
@@ -5,8 +5,10 @@ from PIL import Image, ImageTk
 from valuechange import cb_compatible
 import os
 
+#show the variations with the severities of color blindness
 def color_to_cb(c_type,filename):
     a = 1
+    #conversion matrices in RGB
     Neutral = (1, 0,0 , 0, 0, 1, 0, 0, 0, 0, 1, 0)
     Deuteranomaly0_5 = (0.547494, 0.607765, -0.155259, 0, 0.181692, 0.781742, 0.036566, 0, -0.010410, 0.027275, 0.983136, 0)
     Deuteranomaly1 = (.367322, .860646, -.227968 , 0, .280085, .672501, .047413, 0, -.01182, .04294, .968881, 0)
@@ -15,6 +17,7 @@ def color_to_cb(c_type,filename):
     Tritanomaly0_5 = (1.017277, 0.027029, -0.044306, 0, -0.006113, 0.958479, 0.047634, 0, 0.006379, 0.248708, 0.744913, 0)
     Tritanomaly1 = (1.255528, -0.076749, -0.178779, 0, -0.078411, 0.930809, 0.147602, 0, 0.004733, 0.691367, 0.303900, 0)
     im = (Image.open(filename))
+    #if statements for the type, create 3 images to preview
     if(c_type == "Deuteranomaly"):
         out = im.convert("RGB", Neutral, None, 0,256)
         out = out.save(str(a) + '.jpg')
@@ -42,6 +45,8 @@ def color_to_cb(c_type,filename):
         out = im.convert("RGB", Tritanomaly1, None, 0,256)
         a += 1
         out = out.save(str(a) + '.jpg')
+
+#called by inclusive modification button in interface, takes the hue shifted picture and converts with color matrix
 def finalConversion(imgaging, c_type):
     Deuteranomaly0_5 = (0.547494, 0.607765, -0.155259, 0, 0.181692, 0.781742, 0.036566, 0, -0.010410, 0.027275, 0.983136, 0)
     Protanomaly0_5 = (0.458064, 0.679578, -0.137642, 0, 0.092785, 0.846313, 0.060902, 0, -0.007494, -0.016807, 1.024301, 0)
@@ -58,6 +63,7 @@ def finalConversion(imgaging, c_type):
     if c_type == "Tritanomaly": 
         out = imgaging.convert("RGB", Tritanomaly0_5, None, 0,256)
         out = out.save('inclusive.jpg')
+    #gets rid of the preview pictures and exits
     os.remove("1.jpg")
     os.remove("2.jpg")
     os.remove("3.jpg")
@@ -65,7 +71,7 @@ def finalConversion(imgaging, c_type):
     
     
 
-
+#open directory
 def browseFiles():
     
     filename = filedialog.askopenfilename(initialdir = "/",
@@ -74,27 +80,32 @@ def browseFiles():
       
     # Change label contents
     # label_file_explorer.configure(text="File Opened: "+filename)
+
+    #buttons to call for what type of color blindness
     buttonDeut = tk.Button(window, text="Deuteranomaly",command=lambda: open("Deuteranomaly", filename))
     
     buttonProt = tk.Button(window, text="Protanomaly",command=lambda: open("Protanomaly",filename))
    
     buttonTrit = tk.Button(window, text="Tritanomaly",command=lambda: open("Tritanomaly",filename))
-    buttonDeut.grid(row=1,column = 0)
-    buttonProt.grid(row=2,column = 0)
-    buttonTrit.grid(row=3,column = 0)
+    buttonDeut.place( x = 220 , y =300, height = 50, width = 200)
+    buttonProt.place(x = 430 , y =300, height = 50, width = 200)
+    buttonTrit.place(x = 640 , y =300, height = 50, width = 200)
     
 
-
+#preview window of the variations of intensity
 def open(c_type,filename):
     global my_label
     global img
+    img = ImageTk.PhotoImage(Image.open(filename))
+    # width, height = (Image.open(filename)).size
     top = tk.Toplevel()
     top.title('output')
+    # top.geometry("%ix%i" % (width, height))
     # heading = tk.Label(top, text= " pics")
     # heading.grid(row = 0, column =0)
-    img = ImageTk.PhotoImage(Image.open(filename))
+    
     my_label = tk.Label(top, image=img)
-    my_label.grid(row=0,column =0, columnspan=3)
+    my_label.grid(row=0,column =0, columnspan = 1)
     color_to_cb(c_type,filename)
     img1 = ImageTk.PhotoImage(Image.open('1.jpg'))
     img2 = ImageTk.PhotoImage(Image.open('2.jpg'))
@@ -102,7 +113,7 @@ def open(c_type,filename):
 
     image_list = [img1,img2,img3]
   
-
+    #give functionallity to forwards button
     def forward(num):
         global my_label
         global button_forward
@@ -120,7 +131,8 @@ def open(c_type,filename):
         my_label.grid(row=0,column =0, columnspan=3)
         button_back.grid(row = 1, column = 0)
         button_forward.grid(row = 1, column = 2)
-        
+    
+    #functionallity for back button
     def back(num):
         global my_label
         global button_forward
@@ -137,12 +149,14 @@ def open(c_type,filename):
         my_label.grid(row=0,column =0, columnspan=3)
         button_back.grid(row = 1, column = 0)
         button_forward.grid(row = 1, column = 2)
+    #calls cb_compatible function from valuechange.py. has algorithm for the conversion after previews, will be the hue shifted pics
     def call_cb_compatible(color):
         global im
         global show
         global converted_img
         im = Image.open(filename)
         show = cb_compatible(im, color)
+        #save the hue shifted picture just in case
         show.save("color_adjusted.jpg")
         converted_img = ImageTk.PhotoImage(show)
         top2 = tk.Toplevel()
